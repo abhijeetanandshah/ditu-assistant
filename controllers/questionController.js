5edfd73d9845b81803cc5c2341abaf7041af9453
abhijeetanandshah <35862254+abhijeetanandshah@users.noreply.github.com>
2018-10-14 20:23:24 +0530

    Abhijeet | Bug Fixed

--- controllers/questionController.js
+++ controllers/questionController.js
@@ -1,8 +1,21 @@
 var question = require('../models/question');
+var app = require('../app');
+var mongoose= require('mongoose');
+var async = require('async');
+var db = mongoose.connection;
 
 // Display list of all Questions.
 exports.question_list = function(req, res) {
-    res.send('NOT IMPLEMENTED: Question list');
+    // async.parallel({questions_count : function(callback){
+    //     question.countDocuments({},callback);
+    // }},function(err,results){
+    //     res.render('question',{error:err, data:results});
+    //     console.log("No. of questions : "+results.questions_count);
+    // });
+    db.collection('question').find({}).count((err, docs)=>{
+        res.render('question',{'error':err,'data':docs});
+        console.log("No. of questions : "+ docs);
+    });
 };
 
 // Display detail page for a specific Question.
