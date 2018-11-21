8252278c43f5ea29b84990bba140918f80dbfe49
adityamytech <30368871+adityamytech@users.noreply.github.com>
2018-11-21 10:57:51 +0530

    Aditya | Authentication

--- app.js
+++ app.js
@@ -40,8 +40,8 @@ app.use(passport.session());
 
 
 //Set up default mongoose connection
-var mongoDB ="mongodb://127.0.0.1:27017/ditu-assistant";
-//  var mongoDB ="mongodb://abhijeet.anand99:ditu2018@ds131753.mlab.com:31753/ditu-assistant";
+// var mongoDB ="mongodb://127.0.0.1:27017/ditu-assistant";
+ var mongoDB ="mongodb://abhijeet.anand99:ditu2018@ds131753.mlab.com:31753/ditu-assistant";
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 // Get Mongoose to use the global promise library
 mongoose.Promise = global.Promise;
